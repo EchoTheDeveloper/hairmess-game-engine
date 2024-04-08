e424895b041d55f3f603debeb174624143b7b7ca
Echo <60583177+EchoTheDeveloper@users.noreply.github.com>
2024-04-08 11:05:50 +0100

    i changed alot of stuff

--- assets/setup/example/scenes/espagl.py
+++ assets/setup/example/scenes/espagl.py
@@ -47,6 +47,11 @@ class ESPAGL:
         elif shape_type == Shape.CIRCLE:
             pygame.draw.circle(self.screen, color, (position.x, position.y), 50)  # Draw a circle (radius 50)
 
+    def render_sprite(self, sprite_file_location, sprite_pos_x=0, sprite_pos_y=0, sprite_scale_x=2, sprite_scale_y=2):
+        img = pygame.image.load(sprite_file_location)
+        img = pygame.transform.scale(img, (sprite_scale_x, sprite_scale_y))
+        self.screen.blit(img, (sprite_pos_x, sprite_pos_y))
+
     def write_text(self, text, position, color=(255, 255, 255)):
         font = pygame.font.Font(None, 36)  # Load a default font with size 36
         text_surface = font.render(text, True, color)  # Render the text onto a surface
